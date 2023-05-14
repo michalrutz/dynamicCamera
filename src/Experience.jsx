5ce5ae5a093ac41389e79f11c675e42dc78664e8
Michal Rutz <michal.rutz@gmail.com>
2023-05-14 22:19:43 +0200

    smooth rotation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,14 +3,15 @@ import { easing } from "maath";
 import { useEffect, useRef, useState } from "react";
 
 export function Expereience() {
-  const { camera } = useThree()
-  const meshRef = useRef()
+  const { camera, viewport } = useThree()
+  const { width, height } = viewport;
 
   const [ newCameraPosition, cameraSetPosition ] = useState(camera.position)
 
-  function handleClick (mesh) {
+
+  function changeCamXYZtoMeshXYZ (mesh) {
     // Focus the camera on the mesh
-    cameraSetPosition( [mesh.position.x, mesh.position.y, (mesh.position.z)-5] ) 
+    cameraSetPosition( [mesh.position.x, mesh.position.y, (mesh.position.z)-5] );
   };
 
   useFrame(
@@ -20,27 +21,48 @@ export function Expereience() {
         newCameraPosition,
         0.3,
         delta
-      )
+      );
     }
   )
 
   return (
     <>
-      <Object1 handleClick = { handleClick } position = {[2,1, 0]} color={"red"}/>
-      <Object1 handleClick = { handleClick } position = {[3,2,-2]} color={"green"}/>
-      <Object1 handleClick = { handleClick } position = {[0,1,-4]} color={"blue"}/>
+      <Object1 handleClick = { changeCamXYZtoMeshXYZ } position = {[width/4+1, 1, 0]} color={"red"}/>
+      <Object1 handleClick = { changeCamXYZtoMeshXYZ } position = {[width/4, height/4,-2]} color={"green"}/>
+      <Object1 handleClick = { changeCamXYZtoMeshXYZ } position = {[width/4+2,1,-4]} color={"blue"}/>
     </>
   );
 }
 
 function Object1 ({handleClick, position, color}) {
+
   const meshRef = useRef()
+  const [ newRotation, setRotation ] = useState( [10,0,0] )
+
+  function rotateMesh (mesh) {
+    setRotation( [mesh.rotation.x+10, 0, 0] )
+  }
+
+  useFrame( (s, d)=>{
+    easing.dampE (
+      meshRef.current.rotation,
+      newRotation,
+      0.3,
+      d
+    )
+  } )
+
+
 
   return (
     <mesh
-      position = {position}
-      ref={meshRef}
-      onClick={ () => { handleClick(meshRef.current)} } >
+      position    = {position}
+      ref         = {meshRef}
+      onClick     = { () => {
+        handleClick(meshRef.current);
+        rotateMesh(meshRef.current) }
+      }
+    >
         <boxGeometry />
         <meshBasicMaterial color={color} />
       </mesh>
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <Canvas
       camera={
-        { position : [0,0,-10], fov: 45 } 
+        { position : [0,0,-10],
+          fov: 35
+        } 
       }
       shadows
       eventSource={document.getElementById('root')}
