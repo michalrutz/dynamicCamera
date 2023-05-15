ebbe5f588e26e148f88e4e4199d54f7006b580f3
Michal Rutz <michal.rutz@gmail.com>
2023-05-15 15:11:40 +0200

    card rows generator

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,25 @@
+import { ContactShadows, Html } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { easing } from "maath";
 import { useEffect, useRef, useState } from "react";
+import { DoubleSide } from "three";
+
+
+const card = {
+  width:0.9,
+  height:1.4
+}
 
 export function Expereience() {
   const { camera, viewport } = useThree()
   const { width, height } = viewport;
 
   const [ newCameraPosition, cameraSetPosition ] = useState(camera.position)
-
+  const rectRef = useRef();
 
   function changeCamXYZtoMeshXYZ (mesh) {
     // Focus the camera on the mesh
-    cameraSetPosition( [mesh.position.x, mesh.position.y, (mesh.position.z)-5] );
+    cameraSetPosition( [0, mesh.position.y*0.2, (mesh.position.z)-15] );
   };
 
   useFrame(
@@ -25,22 +33,68 @@ export function Expereience() {
     }
   )
 
+  useEffect( ()=> { rectRef.current.lookAt([0,0.5,0]) }, [])
+
+  const emojis = ["🏀","⚽", "🏐","🍍","🍉","🍈","💀","🧠", "🎸","🧭","🚀", "🏀","⚽", "🏐","🍍","🍉","🍈","💀","🧠", "🎸","🧭","🚀","💡","💡"];
+  const cardsPerRow = 8;
+
+  function xPos (index, cardsPerRow, gap ) {
+    let x = (card.width + index) - (cardsPerRow+1*card.width+gap)*0.5
+    return x 
+  }
+  function yPos (params) {
+    let y = (card.height + index) - (cardsPerRow+1*card.width+gap)*0.5
+    return y 
+  }
+
+  let row = 0
+  let col = Math.floor( emojis.length / cardsPerRow )/2
+  console.log(row)
+
   return (
     <>
-      <Object1 handleClick = { changeCamXYZtoMeshXYZ } position = {[width/4+1, 1, 0]} color={"red"}/>
-      <Object1 handleClick = { changeCamXYZtoMeshXYZ } position = {[width/4, height/4,-2]} color={"green"}/>
-      <Object1 handleClick = { changeCamXYZtoMeshXYZ } position = {[width/4+2,1,-4]} color={"blue"}/>
+      <ambientLight intensity={0.1}/>
+      <spotLight color={"pink"}  intensity={0.5} position={[ 5, 0.2, -10]} ref={rectRef} />
+      <spotLight color={"red"}   intensity={0.5} position={[-5, 0.2, -5]} ref={rectRef} />
+      <spotLight color={"blue"}  intensity={0.5} position={[ 5, 0.2, -5]} ref={rectRef} />
+
+       {emojis.map( (e,i,a) => {
+          if (i%cardsPerRow===0 & i!==0){ row = row+1 } 
+          return <Object1
+            handleClick = { changeCamXYZtoMeshXYZ }
+            position = {[ 
+              xPos(i, cardsPerRow, 0.1) - row*cardsPerRow , 
+              col - (row*1.075* card.height),  
+              0
+            ]}
+            color = {"orange"}
+            emoji = { e }
+          />
+        })}
+
+
+      <mesh
+        position = {[ 0, 0, 0.5]}
+        rotateX={Math.PI*2}
+      >
+        <boxGeometry args={[10,10,0.1]} />
+        <meshStandardMaterial color={"purple"} roughness={0.5}/>
+      </mesh>
     </>
   );
 }
 
-function Object1 ({handleClick, position, color}) {
+function Object1 ({handleClick, position, color, emoji}) {
 
   const meshRef = useRef()
-  const [ newRotation, setRotation ] = useState( [10,0,0] )
+  const [ newRotation, setRotation ] = useState( [0,0,0] )
+  const [ isActive, setIsActive ] = useState( false )
 
   function rotateMesh (mesh) {
-    setRotation( [mesh.rotation.x+10, 0, 0] )
+    let x = mesh.rotation.x-Math.PI;
+    let y = mesh.rotation.y-Math.PI;
+    let z = mesh.rotation.z-Math.PI;
+    setRotation( [0, y, 0] )   
   }
 
   useFrame( (s, d)=>{
@@ -53,18 +107,42 @@ function Object1 ({handleClick, position, color}) {
   } )
 
 
-
   return (
+    <>
     <mesh
+      castShadow
+      recieveShadow
       position    = {position}
       ref         = {meshRef}
-      onClick     = { () => {
-        handleClick(meshRef.current);
-        rotateMesh(meshRef.current) }
+      onClick     = { (event) => { event.stopPropagation( )
+        if (!isActive){
+          setIsActive(true)
+          handleClick(meshRef.current);
+          rotateMesh(meshRef.current);
+          setTimeout(() => {
+            setIsActive(false)
+          }, 1200 )
+        }
+
+      }
       }
     >
-        <boxGeometry />
-        <meshBasicMaterial color={color} />
+        <boxGeometry args={[card.width,card.height, 0.05]} />
+        <meshStandardMaterial
+          color={color}
+          side={DoubleSide}
+          roughness={0.4}
+          metalness={0.5}
+          
+        />
+        <Html 
+          center occlude transform wrapperClass="emoji"
+          position={[0,0,0.2]}
+
+        >
+          {emoji}
+        </Html>
       </mesh>
+      </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import './styles.css'
 import Overlay from './Overlay'
 import { Expereience } from './Experience'
 import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <Canvas
       camera={
-        { position : [0,0,-10],
+        { position : [0, 0.1,-10],
           fov: 35
         } 
       }
